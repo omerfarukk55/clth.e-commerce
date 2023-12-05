d98c85c1e0f109e28e34af1c0b1b592acb756845
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2023-12-05 18:55:58 +0300

    last commit

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,8 +1,7 @@
 import "./authentication.styles.scss";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import SignInForm from "../../components/sign-in-form/sign-in-form.component";
-
+import SignInForm from "../../components/sign-in-form/sign-in.component";
 const Authentication = () => {
  
   return (
