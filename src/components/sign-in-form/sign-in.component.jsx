30be66449d4981d9f06b8f2aa04b1de57c07c62f
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2023-12-08 19:48:26 +0300

    change modifer2

--- src/components/sign-in-form/sign-in.component.jsx
+++ src/components/sign-in-form/sign-in.component.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import {
-  createUserDocumentFromAuth,
-  signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInAuthUserWithEmailAndPassword,
+    signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form.input.component";
 import "./sign-in-form.scss";
@@ -9,65 +10,75 @@ import Button from "../button/button.component";
 
 
 const defaultFormFields = {
- 
-  email: "",
-  password: "",
+
+    email: "",
+    password: "",
 
 };
 
 const SignInForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
-  const {  email, password } = formFields;
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
+    const { email, password } = formFields;
 
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Etkinlik nesnesini almak için fonksiyon parametresini ekledik
-    
-    try {
-      resetFormFields();
-      console.log("User created successfully");
-    } catch (error) {
-     
-    }
-  };
-  const signInWithGoogle = async()=>{
-    const {user} = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+    const handleSubmit = async(event) => {
+        event.preventDefault(); // Etkinlik nesnesini almak için fonksiyon parametresini ekledik
 
-    setFormFields({ ...formFields, [name]: value });
-  };
-  return (
-    <div className="sign-in-container">
-      <span>Already have an account </span>
-      <h2>Sign in with your email and password</h2>
-      <form onSubmit={handleSubmit}>
-        <FormInput
-        label="Email"
-          type="email"
-          required
-          onChange={handleChange}
-          name="email"
-          value={email}
-        />
-        <FormInput
-        label="Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="password"
-          value={password}
-        />
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            console.log(response);
+            resetFormFields();
+        } catch (error) {
+            switch (error.code) {
+                case ('auth/wrong-password'):
+                    alert('yanlış şifre');
+                case ('auth/invalid-login-credentials'):
+                    alert('kullanıcı bulunamadı');
+                default:
+                    console.log(error);
+            }
+        }
+    };
+    const signInWithGoogle = async() => {
+        const { user } = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user);
+    };
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-        <Button  type="submit">Sign In</Button>
-       
-      </form>
-      <Button  oneClick = {signInWithGoogle}>Google Sign In</Button>
-    </div>
-  );
+        setFormFields({...formFields, [name]: value });
+    };
+    return ( <
+        div className = "sign-in-container" >
+        <
+        span > Already have an account < /span> <
+        h2 > Sign in with your email and password < /h2> <
+        form onSubmit = { handleSubmit } >
+        <
+        FormInput label = "Email"
+        type = "email"
+
+        onChange = { handleChange }
+        name = "email"
+        value = { email }
+        /> <
+        FormInput label = "Password"
+        type = "password"
+
+        onChange = { handleChange }
+        name = "password"
+        value = { password }
+        /> <
+        div className = "buttons-container" >
+        <
+        Button type = "submit" > giriş < /Button> <
+        Button buttonType = 'google'
+        onClick = { signInWithGoogle } > Google Sign In < /Button> <
+        /div> <
+        /form> <
+        /div>
+    );
 };
 export default SignInForm;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,99 +1,95 @@
 import { useState } from "react";
 import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth,
+    createAuthUserWithEmailAndPassword,
+    createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form.input.component";
 import "./sign-up-form.styles.scss";
 import Button from "../button/button.component";
 
 const defaultFormFields = {
-  displayName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
 };
 
 const SignUpForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
-  const { displayName, email, password, confirmPassword } = formFields;
-  console.log(formFields);
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Etkinlik nesnesini almak için fonksiyon parametresini ekledik
-    if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
-    }
-    try {
-      const { user } = await createAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
-      await createUserDocumentFromAuth(user, { displayName });
-      resetFormFields();
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
+    const { displayName, email, password, confirmPassword } = formFields;
+    console.log(formFields);
+    const handleSubmit = async(event) => {
+        event.preventDefault(); // Etkinlik nesnesini almak için fonksiyon parametresini ekledik
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email,
+                password
+            );
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
 
-      console.log("User created successfully");
-    } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
-        alert("cannot create user,email already in use");
-      } else {
-        console.log("User creation encountered an error", error);
-      }
-    }
-  };
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+            console.log("User created successfully");
+        } catch (error) {
+            if (error.code === "auth/email-already-in-use") {
+                alert("cannot create user,email already in use");
+            } else {
+                console.log("User creation encountered an error", error);
+            }
+        }
+    };
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    setFormFields({ ...formFields, [name]: value });
-  };
-  return (
-    <div className="sign-up-container">
-    <h2>Don't have an account?</h2>
-      <span>Sign up with your email and password</span>
-      <form onSubmit={handleSubmit}>
-        
-        <FormInput
-          label="Display Name"
-          type="test"
-          required
-          onChange={handleChange}
-          name="displayName"
-          value={displayName}
+        setFormFields({...formFields, [name]: value });
+    };
+    return ( <
+        div className = "sign-up-container" >
+        <
+        h2 > Don 't have an account?</h2> <
+        span > Sign up with your email and password < /span> <
+        form onSubmit = { handleSubmit } >
+
+        <
+        FormInput label = "Display Name"
+        type = "test"
+        required onChange = { handleChange }
+        name = "displayName"
+        value = { displayName }
         />
 
-        
-        <FormInput
-        label="Email"
-          type="email"
-          required
-          onChange={handleChange}
-          name="email"
-          value={email}
+
+        <
+        FormInput label = "Email"
+        type = "email"
+        required onChange = { handleChange }
+        name = "email"
+        value = { email }
         />
 
-        
-        <FormInput
-        label="Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="password"
-          value={password}
-        />
-        <FormInput
-        label = "Confirm Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="confirmPassword"
-          value={confirmPassword}
-        />
-        <Button  type="submit">Sign Up</Button>
-      </form>
-    </div>
-  );
+
+        <
+        FormInput label = "Password"
+        type = "password"
+        required onChange = { handleChange }
+        name = "password"
+        value = { password }
+        /> <
+        FormInput label = "Confirm Password"
+        type = "password"
+        required onChange = { handleChange }
+        name = "confirmPassword"
+        value = { confirmPassword }
+        /> <
+        Button type = "submit" > kayıt ol < /Button> <
+        /form> <
+        /div>
+    );
 };
 export default SignUpForm;
