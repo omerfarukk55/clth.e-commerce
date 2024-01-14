d2d2d834ce2ec827901a3607f77f99370ff597d1
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2024-01-14 10:25:08 +0300

    sepet kontrol

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,6 @@
 
 import { useContext } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 import React from 'react';
@@ -11,14 +11,18 @@ import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const {cartItems} = useContext(CartContext);
+  const navigate = useNavigate();
 
+  const goToCheckouthandler = () =>{
+    navigate('/checkout')
+  }
 
   return (
     <div className='cart-dropdown-container'>
     <div className='cart-items'>
         {cartItems.map(item => <CartItem key= {item.id} cartItem = {item}/>)}
     </div>  
-    <Button>SEPETİ KONTROL ET</Button>
+    <Button onClick = {goToCheckouthandler}>  SEPETİ KONTROL ET </Button>
     </div>
   )
 }
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './checkout.style.scss';
+
+ const Checkout = () => {
+  return (
+    <div>
+    <h1> ı am the checkout pages</h1>
+    </div>
+  )
+}
+
+export default Checkout;
