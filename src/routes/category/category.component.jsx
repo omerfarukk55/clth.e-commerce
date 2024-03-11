a8f7a3897eeecbf439710737fd42d3e475254a40
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2024-03-11 20:43:07 +0300

    finish day

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
@@ -16,12 +16,16 @@ const Category = () => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
   return (
+    <Fragment>
+    <h2 className="category-title">{category.toUpperCase()}</h2>
     <div className="category-container">
-      {products && 
-        products.map((product) => (
+    
+    {products && 
+      products.map((product) => (
         <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+        ))}
+        </div>
+        </Fragment>
   );
 };
 
