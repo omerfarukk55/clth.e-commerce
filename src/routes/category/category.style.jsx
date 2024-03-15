7ee6c820562cf593e135b9491315eebe3431e963
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2024-03-15 14:35:32 +0300

    new atribute

--- src/routes/category/category.style.jsx
+++ src/routes/category/category.style.jsx
@@ -5,7 +5,7 @@ export const CategoryContainer = styled.div`
 display: grid;
 grid-template-columns: repeat(4, 1fr);
 column-gap: 20px;
-row-gap: 50ps;
+row-gap: 50px;
 `;
 export const CategoryTitle = styled.h2`
     font-size: 38px;
